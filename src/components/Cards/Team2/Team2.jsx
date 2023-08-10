279c0f330a536fa21f50cdd9cdd546a35aad8816
dmolins <damian.bravo.molins@gmail.com>
2023-08-10 08:04:01 -0300

    Card API-2

--- src/components/Cards/Team2/Team2.jsx
+++ src/components/Cards/Team2/Team2.jsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Modal } from 'react-bootstrap';
 import { faSliders } from '@fortawesome/free-solid-svg-icons';
+import { Peliculas } from './Peliculas';
+import { SliderPeliculas } from './SliderPeliculas';
+import './team2.css';
 
 export const Team2 = () => {
-  const [respuesta, setRespuesa] = useState(undefined);
+  const [peliculas, setPeliculas] = useState([]);
   const [showSettingModal, setShowSettingModal] = useState(false);
   const handleShowSettingModal = () => setShowSettingModal(true);
   const handleCloseSettingModal = () => setShowSettingModal(false);
 
   useEffect(() => {
     const llamada = async () => {
-      let response = await fetch('http://localhost:8080/api/greeting');
+      let response = await fetch('http://localhost:8082/api2/movies/comingsoon/AR');
       let data = await response.text();
-      setRespuesa(data);
+      setPeliculas(JSON.parse(data));
     };
     llamada();
   }, []);
@@ -21,38 +24,22 @@ export const Team2 = () => {
   return (
     <>
       <div className="card">
+        <SliderPeliculas peliculas={peliculas} />
         <FontAwesomeIcon
           className="fw-bold fs-5 position-absolute"
           style={{ right: '2rem', cursor: 'pointer' }}
           icon={faSliders}
           onClick={handleShowSettingModal}
         ></FontAwesomeIcon>
-        <p>{respuesta}</p>
       </div>
 
       {/* MODAL EDITABLE PARA CADA TEAM */}
       <Modal show={showSettingModal} onHide={handleCloseSettingModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Configuracion</Modal.Title>
+          <Modal.Title>Buscar Peliculas</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {/* Aqui van la interfaz front dependiendo de la funcionalidad, en caso de que necesiten ayuda con esta parte porfavor avisarme (German).*/}
-          <form className="my-form">
-            <div className="boxInput">
-              <label htmlFor="campo1">Campo 1</label>
-              <input type="text" id="campo1" />
-            </div>
-            <div className="boxInput">
-              <label htmlFor="campo2">Campo 2</label>
-              <input type="text" id="campo2" />
-            </div>
-            <div className="boxInput">
-              <label htmlFor="campo3">Campo 3</label>
-              <input type="text" id="campo3" />
-            </div>
-            <button>Enviar</button>
-          </form>
-          {/* Este form es solo para ejemplificar, como mencione, se puede cambiar dependediendo del proyecto */}
+          <Peliculas />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseSettingModal}>
